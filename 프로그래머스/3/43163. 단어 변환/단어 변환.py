fccba30efeb55b86b6d162c52c5e549720b4603c
genie0000 <genie001231@naver.com>
2025-04-14 21:23:16 +0900

    [level 3] Title: 단어 변환, Time: 0.43 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,11 +1,16 @@
+# deque는 양방향 큐 이고, 일반 리스트보다 pop, append가 더 빠르기 때문에 BFS구현에 자주 사용된다.
+from collections import deque
+
 def solution(begin, target, words):
-    # min_steps는 최소 단계 수를 추적할 리스트, 초기값은 무한대로 설정
-    min_steps = [float('inf')]
-    # words 리스트에서 각 단어가 사용됐는지 여부를 저장(중복방지를 위함)
+    # target 단어가 words안에 없으면 바로 0 반환
+    if target not in words:
+        return 0
+    
+    # BFS탐색을 위한 큐에 시작단어와 변환 횟수를 튜플 형태로 넣는다.
+    queue = deque([(begin, 0)])
     visited = [False] * len(words)
     
-    # 두 단어를 비교해서 한 글자만 다른지 확인하는 함수
-    def one_letter_diff(a, b):
+    def one_letter_diff(a,b):
         diff = 0
         for i in range(len(a)):
             if a[i] != b[i]:
@@ -14,23 +19,18 @@ def solution(begin, target, words):
             return True
         return False
     
-    # 재귀함수 정의, current_word(현재 단어), steps(지금까지 변환한 횟수)
-    def dfs(current_word, steps):
+    # 큐에 요소가 있을 동안 계속 반복
+    while queue:
+        # 큐에서 현재 단어와 변환 횟수를 꺼낸다.
+        # popleft()는 큐의 맨 앞에서 꺼내는 함수이다.)
+        current_word, steps = queue.popleft()
         if current_word == target:
-            min_steps[0] = min(min_steps[0], steps)
-            return
-        
+            return steps
         for i in range(len(words)):
             if not visited[i] and one_letter_diff(current_word, words[i]):
                 visited[i] = True
-                dfs(words[i], steps + 1)
-                visited[i] = False
-    
-    # 시작 단어에서부터 DFS 탐색 시작, 단계 수는 0
-    dfs(begin,0)
-    
-    # min_steps가 여전히 무한대면 0을 그게 아니면 min_steps[0]을 반환한다.
-    return 0 if min_steps[0] == float('inf') else min_steps[0]
+                queue.append((words[i], steps + 1))
+    return 0
+        
     
     
-    return answer
