c78663acc02756f03f4ef0755fc34fe73e6136c8
genie0000 <genie001231@naver.com>
2025-04-16 23:42:21 +0900

    [level 3] Title: 여행경로, Time: 0.20 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.py
+++ 프로그래머스/3/43164. 여행경로/여행경로.py
@@ -1,19 +1,22 @@
+import heapq
+
 def solution(tickets):
-    answer = []
-    tickets.sort()
-    used = [False] * len(tickets)
+    queue = []
+    heapq.heappush(queue, (["ICN"], tickets))
     
-    def dfs(route, depth):
-        if depth == len(tickets):
-            answer.append(route[:])
-            return
-        for i in range(len(tickets)):
-            if not used[i] and route[-1] == tickets[i][0]:
-                used[i] = True
-                dfs(route + [tickets[i][1]], depth+ 1)
-                used[i] = False
-    
-    dfs(["ICN"],0)
+    while queue:
+        route, remain = heapq.heappop(queue)
         
+        if not remain:
+            return route
     
-    return answer[0]
+        # 현재 마지막으로 추가된 공항 위치
+        last = route[-1]
+
+        for i, (start, end) in enumerate(remain):
+            if start == last:
+                new_route = route + [end]
+                # i번째 항공권만 제거한 새로운 remain리스트
+                new_remain = remain[:i] + remain[i+1:]
+                # 새로운 경로와 남은 항공권 리스트를 우선순위 큐에 넣는다.
+                heapq.heappush(queue, (new_route, new_remain))
